5e12572661ebc3c96d97154ebd4a671d12471c9f
John-Paul Navarro <navarro@anl.gov>
2022-01-21 08:40:58 -0600

    Finish disabling hourly views

--- database/refresh_materialized_views.sql
+++ database/refresh_materialized_views.sql
@@ -1,4 +1,4 @@
 \timing on
 REFRESH MATERIALIZED VIEW sum_usage_user_monthly  WITH DATA;
 REFRESH MATERIALIZED VIEW sum_usage_user_daily    WITH DATA;
-REFRESH MATERIALIZED VIEW sum_usage_user_hourly   WITH DATA;
+-- REFRESH MATERIALIZED VIEW sum_usage_user_hourly   WITH DATA;
