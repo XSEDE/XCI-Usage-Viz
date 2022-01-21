ec3a15a823310a25f6148892e2fd97f419631779
John-Paul Navarro <navarro@anl.gov>
2022-01-21 10:01:37 -0600

    Remove accidental cruft in refresh_materialized_views

--- database/refresh_materialized_views.sql
+++ database/refresh_materialized_views.sql
@@ -6,12 +6,3 @@ REFRESH MATERIALIZED VIEW sum_usage_user_client_daily    WITH DATA;
 REFRESH MATERIALIZED VIEW sum_usage_monthly              WITH DATA;
 REFRESH MATERIALIZED VIEW sum_usage_daily                WITH DATA;
 -- REFRESH MATERIALIZED VIEW sum_usage_user_hourly   WITH DATA;
-
-
-CREATE MATERIALIZED VIEW sum_usage_user_monthly (used_component, use_month, use_user, use_count, use_amount, use_amount_units)
-CREATE MATERIALIZED VIEW sum_usage_user_daily (used_component, use_day, use_user, use_count, use_amount, use_amount_units)
-CREATE MATERIALIZED VIEW sum_usage_monthly (used_component, use_month, use_count, use_distinct_users, use_distinct_clients, use_amount, use_amount_units)
-CREATE MATERIALIZED VIEW sum_usage_daily (used_component, use_day, use_count, use_distinct_users, use_distinct_clients, use_amount, use_amount_units)
-CREATE MATERIALIZED VIEW sum_usage_user_client_monthly (used_component, use_month, use_user, use_client, use_count)
-CREATE MATERIALIZED VIEW sum_usage_user_client_daily (used_component, use_day, use_user, use_client, use_count)
-
